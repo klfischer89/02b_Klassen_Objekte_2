9c280ed826695581b5a053414326d4b77ef18575
klfischer89 <fischer-k4@web.de>
2025-11-24 09:27:50 +0100

    usage of a setter

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -6,8 +6,8 @@ public class App {
 
         // Instanziierung + Wertzuweisung
         Cat cat = new Cat("Grizabella", "white", 29);
-        output("Blick von aussen: " + cat);
-        output("Blick von innen: " + cat.getInstanceVariable());
+        // output("Blick von aussen: " + cat);
+        // output("Blick von innen: " + cat.getInstanceVariable());
 
         output(cat.getFirstName());
         output(cat.getFurColor());
@@ -16,13 +16,16 @@ public class App {
         output("-----------------------");
 
         Cat cat2 = new Cat("Alonzo", "grey", 35);
-        output("Blick von aussen: " + cat2);
-        output("Blick von innen: " + cat2.getInstanceVariable());
+        // output("Blick von aussen: " + cat2);
+        // output("Blick von innen: " + cat2.getInstanceVariable());
 
         output(cat2.getFirstName());
         output(cat2.getFurColor());
         output(Integer.toString(cat2.getAge()));  // Explizite Typumwandlung!
 
+        output("-----------------------");
+        cat2.setAge(36);
+        output(Integer.toString(cat2.getAge()));  // Explizite Typumwandlung!
 
 
     }
